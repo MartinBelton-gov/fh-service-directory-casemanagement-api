cba3f14ae87df17a8c595288d9a43ed27f4f88e6
Martin-Belton-gov <martin.belton@digital.education.gov.uk>
2022-09-14 10:44:53 +0100

    Added Additional Api calls for dashboard

--- src/FamilyHubs.ServiceDirectoryCaseManagement.Api/Endpoints/MinimalReferralEndPoints.cs
+++ src/FamilyHubs.ServiceDirectoryCaseManagement.Api/Endpoints/MinimalReferralEndPoints.cs
@@ -39,7 +39,36 @@ public class MinimalReferralEndPoints
                 System.Diagnostics.Debug.WriteLine(ex.Message);
                 throw;
             }
-        }).WithMetadata(new SwaggerOperationAttribute("Get Referrals", "Get Referral By Referrer") { Tags = new[] { "Referrals" } });
+        }).WithMetadata(new SwaggerOperationAttribute("Get Referrals", "Get Referrals By Referrer") { Tags = new[] { "Referrals" } });
 
+        app.MapGet("api/organisationreferrals/{organisationid}", async (string organisationId, int? pageNumber, int? pageSize, CancellationToken cancellationToken, ISender _mediator) =>
+        {
+            try
+            {
+                GetReferralsByOrganisationIdCommand request = new(organisationId, pageNumber, pageSize);
+                var result = await _mediator.Send(request, cancellationToken);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.Message);
+                throw;
+            }
+        }).WithMetadata(new SwaggerOperationAttribute("Get Referrals", "Get Referrals By Organisation Id") { Tags = new[] { "Referrals" } });
+
+        app.MapGet("api/referral/{id}", async (string id, CancellationToken cancellationToken, ISender _mediator) =>
+        {
+            try
+            {
+                GetReferralByIdCommand request = new(id);
+                var result = await _mediator.Send(request, cancellationToken);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.Message);
+                throw;
+            }
+        }).WithMetadata(new SwaggerOperationAttribute("Get Referrals", "Get Referral By Id") { Tags = new[] { "Referrals" } });
     }
 }
---
+++ src/FamilyHubs.ServiceDirectoryCaseManagement.Api/Queries/GetReferrals/GetReferralsByOrganisationIdCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace FamilyHubs.ServiceDirectoryCaseManagement.Api.Queries.GetReferrals;
+
+public class GetReferralsByOrganisationIdCommandValidator : AbstractValidator<GetReferralsByOrganisationIdCommand>
+{
+    public GetReferralsByOrganisationIdCommandValidator()
+    {
+        RuleFor(v => v.OrganisationId)
+            .NotNull()
+            .NotEmpty();
+    }
+}
